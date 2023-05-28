f85b509cf9b9c6789ede70ac85ba97a699613e23
szentesik <sz.krisz@freemail.hu>
2023-05-28 13:42:01 +0200

    Forked for CSTA Inside to experiment with. Moved to VS2022. Added missing dlls to load mod_managed properly on startup. Fixed broken references in Jabra plugin.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FSClient")]
+[assembly: AssemblyTitle("FSClient - built for CSTA Inside")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mitch Capper")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.11.0")]
-[assembly: AssemblyFileVersion("1.4.11.0")]
+[assembly: AssemblyVersion("1.4.12.0")]
+[assembly: AssemblyFileVersion("1.4.12.0")]
